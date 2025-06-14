85a405c8d351bc5fa5a36a58a8314a941e058e61
Khylko Andrii <hilko_a@itstep.academy>
2025-06-14 11:21:23 +0300

    Initial commit

---
+++ diff.cpp
@@ -0,0 +1,6 @@
+#include "diff.h"
+
+int diff(int a, int b)
+{
+	return a-b;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "sum.h"
+#include "diff.h"
+#include <cmath>
+#include "prod.h"
+
+using namespace std;
+int main()
+{
+	cout<<"Hello world!!!"<<endl;
+	cout<<"Sum = "<<sum(12,23)<<endl;
+	cout<<"Difference = "<<diff(12,23)<<endl;
+	cout<<"Power = "<<pow(2,3)<<endl;
+	cout<<"Product = "<<prod(12,23)<<endl;
+}	
